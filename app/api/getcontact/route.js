880969fa12e920bad9cec937fa809b6958a0793f
Jeedug <131798215+Jeedug@users.noreply.github.com>
2025-02-28 07:45:24 -0600

    Update route.js

--- app/api/getcontact/route.js
+++ app/api/getcontact/route.js
@@ -1,30 +1,30 @@
 import { db } from "../../models/config"; // Importar la conexión a la base de datos
 import { contactForm } from "../../models/schema"; // Importar el esquema
 
-// Manejador GET para obtener todos los contactos
-export async function GET(request) {
-  try {
-    // Obtener todos los registros de la tabla contact_form
-    const contacts = await db.select().from(contactForm);
+export const dynamic = 'force-dynamic';
 
-    // Retornar los datos como respuesta JSON
-    return new Response(JSON.stringify(contacts), {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  } catch (error) {
-    // Capturar errores y retornar una respuesta de error
-    console.error("Error al obtener los contactos:", error);
-    return new Response(
-      JSON.stringify({ error: "Ocurrió un error al obtener los contactos." }),
-      {
-        status: 500,
+export async function GET() {
+    try {
+      const contacts = await db.select().from(contactForm);
+      console.log("Datos obtenidos:", contacts); // Agrega este log para depurar
+      return new Response(JSON.stringify(contacts), {
+        status: 200,
         headers: {
           "Content-Type": "application/json",
+          "Cache-Control": "no-store, max-age=0", // Deshabilita la caché
         },
-      }
-    );
+      });
+    } catch (error) {
+      console.error("Error al obtener los contactos:", error);
+      return new Response(
+        JSON.stringify({ error: "Ocurrió un error al obtener los contactos." }),
+        {
+          status: 500,
+          headers: {
+            "Content-Type": "application/json",
+            "Cache-Control": "no-store, max-age=0", // Deshabilita la caché
+          },
+        }
+      );
+    }
   }
-}
